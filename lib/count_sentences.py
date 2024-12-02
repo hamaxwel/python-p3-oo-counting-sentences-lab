09482af2e9a1dc52b3cd4d99e66eb1ad09c2c5e5
maxwel kirimi <maxwelkirimii@gmail.com>
2024-12-02 10:40:19 +0300

    assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,32 @@
-#!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+    def __init__(self, value=''):
+        if not isinstance(value, str):
+            print("The value must be a string.")
+        self._value = value if isinstance(value, str) else ''
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if not isinstance(new_value, str):
+            print("The value must be a string.")
+        else:
+            self._value = new_value
+
+    def is_sentence(self):
+        return self._value.endswith(".")
+
+    def is_question(self):
+        return self._value.endswith("?")
+
+    def is_exclamation(self):
+        return self._value.endswith("!")
+
+    def count_sentences(self):
+        standardized_value = re.sub(r'[.!?]+', '.', self._value)
+        sentences = [s.strip() for s in standardized_value.split('.') if s.strip()]
+        return len(sentences)
